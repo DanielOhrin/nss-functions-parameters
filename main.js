3b66a72d27144e8cf46480e1152997f1201ef57b
Daniel Ohrin <xqiam@outlook.com>
2022-10-07 09:44:50 -0500

    Initial

---
+++ main.js
@@ -0,0 +1,10 @@
+const quarterValue = (numToQuarter) => {
+    const numQuartered = numToQuarter / 4;
+    return numQuartered;
+}
+
+// Invoke the function and store the return value
+const quarterThirtyEight = quarterValue(38)
+
+// Log the return value
+console.log(quarterThirtyEight)
